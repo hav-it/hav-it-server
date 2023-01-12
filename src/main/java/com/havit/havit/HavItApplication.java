92b208b6ff11dfb57fcab40f28c738d810373028
Jin409 <sualng123@gmail.com>
2023-01-12 17:57:48 +0900

    initial setting

---
+++ src/main/java/com/havit/havit/HavItApplication.java
@@ -0,0 +1,13 @@
+package com.havit.havit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/havit/havit/HavItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havit.havit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
